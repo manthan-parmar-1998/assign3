e4eff63067021027b373f5507f59ea977b37d738
Manthan Parmar <parmarmanthan1313@gmail.com>
2024-12-10 18:48:04 -0500

    Changes in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Parmar ECS Container."
+    return "Hello from Parmar ECS Container - Changes added for CI/CD"
 
 
 if __name__ == '__main__':
