83b8be009cfb6b08de720feb807453f7a67b3570
Manthan Parmar <parmarmanthan1313@gmail.com>
2024-12-10 19:26:31 -0500

    Changes in app.py - Revert

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Parmar ECS Container - Changes added for CI/CD"
+    return "Hello from Parmar ECS Container"
 
 
 if __name__ == '__main__':
